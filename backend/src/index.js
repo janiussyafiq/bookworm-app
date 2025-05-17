ff4daff8ed36c01e0b845fbbd52091d53acb423f
janiussyafiq <janiussyafiq@wisc.edu>
2025-05-17 20:15:14 +0900

    Added limit using express.json() to fix bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config"
 import job from "./lib/cron.js";
-import bodyParser from "body-parser";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
@@ -15,8 +14,8 @@ job.start();
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json({ limit: "10mb" })); // Adjust the limit as needed
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "10mb" })); // Adjust the limit as needed
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/books", bookRoutes)
--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -41,7 +41,7 @@ export default function Create() {
                 mediaTypes: "images",
                 allowsEditing: true,
                 aspect: [4, 3],
-                quality: 0.5, // lower the qualtiy for smaller base64
+                quality: 0.1, // lower the qualtiy for smaller base64
                 base64: true,
             });
 
